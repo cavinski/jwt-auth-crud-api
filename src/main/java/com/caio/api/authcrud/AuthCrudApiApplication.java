c2ff9caf2f6c7120a32c1fe0c6fb22032713d4a0
cavinski <caiopedrovianadasilva@gmail.com>
2026-05-05 09:25:41 -0300

    Initial commit

---
+++ src/main/java/com/caio/api/authcrud/AuthCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.caio.api.authcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthCrudApiApplication.class, args);
+	}
+
+}
