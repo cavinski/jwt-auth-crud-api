6e46d28582a8df861f31961829fc8209f80b889e
cavinski <caiopedrovianadasilva@gmail.com>
2026-05-06 19:57:47 -0300

    feat: create user DTOs

---
+++ src/main/java/com/caio/api/authcrud/dto/UserRequest.java
@@ -0,0 +1,10 @@
+package com.caio.api.authcrud.dto;
+
+import lombok.Data;
+
+public class UserRequest {
+    
+    private String name;
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/caio/api/authcrud/dto/UserResponse.java
@@ -0,0 +1,10 @@
+package com.caio.api.authcrud.dto;
+
+import lombok.Data;
+
+public class UserResponse {
+    
+    private Long id;
+    private String name;
+    private String email;
+}
