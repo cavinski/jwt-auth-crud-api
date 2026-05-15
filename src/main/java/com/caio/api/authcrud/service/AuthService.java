38429a447c52a23c972f5ab5408601e63f3a449e
Caio Pedro <caiopedrovianadasilva@gmail.com>
2026-05-15 17:50:09 -0300

    feat: define authentication service interface

---
+++ src/main/java/com/caio/api/authcrud/service/AuthService.java
@@ -0,0 +1,13 @@
+package com.caio.api.authcrud.service;
+
+import com.caio.api.authcrud.dto.auth.AuthResponse;
+import com.caio.api.authcrud.dto.auth.LoginRequest;
+import com.caio.api.authcrud.dto.UserRequest;
+
+public interface AuthService {
+    
+    void register(UserRequest request);
+
+    AuthResponse Login(LoginRequest request);
+    
+}
