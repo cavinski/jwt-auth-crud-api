e09b57690634745be352a4f11a355756e05a92da
Caio Pedro <caiopedrovianadasilva@gmail.com>
2026-05-16 20:49:30 -0300

    feat: integrate User entity with Spring Security

--- src/main/java/com/caio/api/authcrud/entity/User.java
+++ src/main/java/com/caio/api/authcrud/entity/User.java
@@ -1,12 +1,17 @@
 package com.caio.api.authcrud.entity;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.GrantedAuthority;
+import java.util.Collection;
+import java.util.List;
+
 import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Table(name = "users")
 @Data
-public class User {
+public class User implements UserDetails{
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,4 +23,19 @@ public class User {
     private String email;
 
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
 }
