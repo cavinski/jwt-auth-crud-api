55371cf3a2995e1c602f6cd35929ccf5b31d7929
Caio Pedro <caiopedrovianadasilva@gmail.com>
2026-05-16 20:42:24 -0300

    feat: implement JWT service

--- src/main/java/com/caio/api/authcrud/security/JwtService.java
+++ src/main/java/com/caio/api/authcrud/security/JwtService.java
@@ -13,19 +13,23 @@ import javax.crypto.SecretKey;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "mysecretkeymysecretkeymysecretkey123456";
     
-    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
 
     public String generateToken(String email) {
+
         return Jwts.builder()
-            .setSubject(email)
-            .setIssuedAt(new Date())
-            .setExpiration(new Date(System.currentTimeMillis() + 86400000))
-            .signWith(key)
-            .compact();
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
     }
 
     public String extractEmail(String token) {
+
         Claims claims = Jwts.parser()
             .verifyWith((SecretKey) key)
             .build()
@@ -33,5 +37,12 @@ public class JwtService {
             .getPayload();
 
         return claims.getSubject();
+    }
+
+    public boolean isTokenValid(String token, String email) {
+
+        String extractedEmail = extractEmail(token);
+
+        return extractedEmail.equals(email);
     }
 }
