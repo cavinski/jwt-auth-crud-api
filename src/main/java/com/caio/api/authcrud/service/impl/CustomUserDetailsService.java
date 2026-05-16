d7c6a82571431794aa411a5bae1fb25fcd477b7e
Caio Pedro <caiopedrovianadasilva@gmail.com>
2026-05-16 20:59:16 -0300

    feat: implement custom UserDetailsService

---
+++ src/main/java/com/caio/api/authcrud/service/impl/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.caio.api.authcrud.service.impl;
+
+import com.caio.api.authcrud.entity.User;
+import com.caio.api.authcrud.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        
+        return userRepository.findByEmail(email)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+    }
+}
