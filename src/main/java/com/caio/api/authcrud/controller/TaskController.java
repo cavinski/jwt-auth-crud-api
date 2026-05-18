b3e40b97202e10dc4b0bf686a7d94af0a5295d9f
Caio Pedro <caiopedrovianadasilva@gmail.com>
2026-05-18 20:04:44 -0300

    feat: expose authenticated user endpoint

--- src/main/java/com/caio/api/authcrud/controller/TaskController.java
+++ src/main/java/com/caio/api/authcrud/controller/TaskController.java
@@ -1,18 +1,15 @@
 package com.caio.api.authcrud.controller;
 
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
+import org.springframework.security.core.Authentication;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
     
-    @GetMapping
-    public List<String> getTasks() {
-        return List.of(
-            "Taks 1",
-            "Task 2",
-            "Task 3"
-        );
-    }
+    @GetMapping("/me")
+    public Map<String,String> me(Authentication authentication) {
+        return Map.of("email", authentication.getName());
+    } 
 }
