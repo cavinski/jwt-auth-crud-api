90a1a91f63d70ddf0f06a7bb9b2fbe659c376de7
Caio Pedro <caiopedrovianadasilva@gmail.com>
2026-05-19 20:28:21 -0300

    feat: create task not found and unauthorized exceptions

--- src/main/java/com/caio/api/authcrud/exception/GlobalExceptionHandler.java
+++ src/main/java/com/caio/api/authcrud/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,5 +22,21 @@ public class GlobalExceptionHandler {
         );
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> hanleNotFound(ResourceNotFoundException ex) {
+
+        return ResponseEntity
+        .status(HttpStatus.NOT_FOUND)
+        .body(Map.of("timestamp", LocalDateTime.now(), "error", ex.getMessage()));
+    }
+
+    @ExceptionHandler(UnauthorizedTaskAccessException.class)
+    public ResponseEntity<?> handleUnauthorized(UnauthorizedTaskAccessException ex) {
+
+        return ResponseEntity
+        .status(HttpStatus.FORBIDDEN)
+        .body(Map.of("timestamp", LocalDateTime.now(), "error", ex.getMessage()));
     }
 }
