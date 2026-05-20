cdadb437b136c693f5e204a13d22940a103714fc
Caio Pedro <caiopedrovianadasilva@gmail.com>
2026-05-19 22:23:52 -0300

    feat: create task CRUD endpoints

--- src/main/java/com/caio/api/authcrud/controller/TaskController.java
+++ src/main/java/com/caio/api/authcrud/controller/TaskController.java
@@ -3,6 +3,7 @@ package com.caio.api.authcrud.controller;
 import com.caio.api.authcrud.dto.*;
 import com.caio.api.authcrud.service.TaskService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -22,4 +23,20 @@ public class TaskController {
     public List<TaskResponseDTO> findAll() {
         return taskService.findAll();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskResponseDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(taskService.findById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TaskResponseDTO> update(@PathVariable Long id, @RequestBody TaskRequestDTO request) {
+        return ResponseEntity.ok(taskService.update(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        taskService.delete(id);
+        return ResponseEntity.noContent().build();
+    } 
 }
