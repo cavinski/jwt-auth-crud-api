84c1c0c7607e4c52b76c19f0990a83a760f1b378
Caio Pedro <caiopedrovianadasilva@gmail.com>
2026-05-22 14:53:51 -0300

    feat: configure application routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register'
+import { Tasks } from './pages/tasks/tasks';
+import { authGuard } from './guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'login', component:Login },
+    { path:'register', component:Register },
+    { path:'tasks', component:Tasks, canActivate:[authGuard]},
+    { path:'', redirectTo:'login', pathMatch:'full' }
+];
