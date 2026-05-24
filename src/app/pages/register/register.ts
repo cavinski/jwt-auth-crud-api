14d1db94da43b64f75439ae632fc088ed61ae9fa
Caio Pedro <caiopedrovianadasilva@gmail.com>
2026-05-24 18:35:11 -0300

    feat: implement register page

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,9 +1,39 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './register.html',
-  styleUrl: './register.css',
 })
-export class Register {}
+
+export class Register {
+
+  name = '';
+  email = '';
+  password = '';
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  register() {
+
+    const data = {
+      name: this.name,
+      email: this.email,
+      password: this.password
+    }
+
+    this.auth.register(data).subscribe({
+      next: () => {
+        this.router.navigate(['/login']);
+      }
+    });
+
+  } 
+
+}
