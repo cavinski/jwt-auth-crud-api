dd92aef698e236bc2ed4c2afa0562811e1880320
Caio Pedro <caiopedrovianadasilva@gmail.com>
2026-05-24 19:14:41 -0300

    feat: add task service integration

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,6 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Task {}
+
+export class Task {
+
+  private api = 'http://localhost:8080/tasks';
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  getTasks(): Observable<any> {
+    return this.http.get(this.api);
+  }
+
+  createTask(task: any) {
+    return this.http.post(this.api, task);
+  }
+
+  deleteTask(id: number) {
+    return this.http.delete(`${this.api}/${id}`);
+  }
+
+}
